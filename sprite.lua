d62b1ef0a7b6e177c1636afab3c59a15da9b214e
Cédric Creusot <calimeraw@RedApple.local>
2015-04-19 16:44:31 +0200

    Add farmer and zombie

--- sprite.lua
+++ sprite.lua
@@ -2,5 +2,7 @@ local sprite = {}
 
 sprite.chicken = "drawing/chicken.png"
 sprite.background = "drawing/battlefield.bmp"
+sprite.zombie = "drawing/zombie.png"
+sprite.farmer = "drawing/farmer.png"
 
 return sprite
