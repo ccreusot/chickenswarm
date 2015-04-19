e555c2a90c65ac8d391b80024b44f639c3345989
Cédric Creusot <calimeraw@RedApple.local>
2015-04-19 22:46:08 +0200

    update seeds

--- sprite.lua
+++ sprite.lua
@@ -4,5 +4,6 @@ sprite.chicken = "drawing/chicken.png"
 sprite.background = "drawing/battlefield.bmp"
 sprite.zombie = "drawing/zombie.png"
 sprite.farmer = "drawing/farmer.png"
+sprite.seed = "drawing/seed.png"
 
 return sprite
